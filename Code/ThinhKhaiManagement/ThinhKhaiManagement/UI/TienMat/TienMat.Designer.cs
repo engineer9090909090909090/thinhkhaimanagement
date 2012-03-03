cc493cf1b831cc55275dec4180877f25cd623eb1
doquoctuong <doquoctuong@gmail.com>
2012-03-03 03:35:02 +0000

    fix tien mat

--- Code/ThinhKhaiManagement/ThinhKhaiManagement/UI/TienMat/TienMat.Designer.cs
+++ Code/ThinhKhaiManagement/ThinhKhaiManagement/UI/TienMat/TienMat.Designer.cs
@@ -198,6 +198,7 @@
             // radSpinEditorTienMat
             // 
             this.radSpinEditorTienMat.BackColor = System.Drawing.Color.White;
+            this.radSpinEditorTienMat.DecimalPlaces = 2;
             this.radSpinEditorTienMat.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.radSpinEditorTienMat.Location = new System.Drawing.Point(185, 57);
             this.radSpinEditorTienMat.Maximum = new decimal(new int[] {
